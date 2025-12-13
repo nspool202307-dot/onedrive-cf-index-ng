7b0a2b95011945ae99f00b5bd51a3044dd9e4c33
nspool202307-dot <nspool202307@gmail.com>
2025-12-13 15:11:43 +0800

    Update api.config.js

--- config/api.config.js
+++ config/api.config.js
@@ -1,33 +1,27 @@
 /**
  * This file contains the configuration for the API endpoints and tokens we use.
- *
- * - If you are a OneDrive International user, you would not have to change anything here.
- * - If you are not the admin of your OneDrive for Business account, you may need to define your own clientId/clientSecret,
- *   check documentation for more details.
- * - If you are using a E5 Subscription OneDrive for Business account, the direct links of your files are not the same here.
- *   In which case you would need to change directLinkRegex.
+ * NOTE: client_id and client_secret are hardcoded here to bypass environment variable injection issues in Cloudflare Pages.
  */
 module.exports = {
-  client_id: 'f20fcec1-00d0-4811-b5a0-7260dffe1728', // <--- 填入 ID
-  client_secret: 'Ns18Q~e7b.D4E00pX5oZGSYi71D0epY.kfRlEbha', // <--- 填入 Secret
+  // 您的正確 CLIENT_ID 和 CLIENT_SECRET (硬編碼)
+  client_id: 'f20fcec1-00d0-4811-b5a0-7260dffe1728',
+  client_secret: 'Ns18Q~e7b.D4E00pX5oZGSYi71D0epY.kfRlEbha',
+
+  // Redirect URI (Step 1/3 頁面顯示的值)
   redirectUri: 'http://localhost',
 
-  // The redirectUri is the URL that the user will be redirected to after they have authenticated with Microsoft Graph API.
-  // Likewise, you would not need to change redirectUri if you are using your personal Microsoft account with OneDrive International.
-  redirectUri: 'http://localhost'
-
-  // These are the URLs of the OneDrive API endpoints. You would not need to change anything here if you are using OneDrive International
-  // or E5 Subscription OneDrive for Business. You may need to change these if you are using OneDrive 世纪互联.
+  // API 端點
   authApi: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
   driveApi: 'https://graph.microsoft.com/v1.0/me/drive',
 
-  // The scope we require are listed here, in most cases you would not need to change this as well.
+  // 權限範圍
   scope: 'user.read files.read.all offline_access',
 
-  // Cache-Control header, check Vercel documentation for more details. The default settings imply:
-  // - max-age=0: no cache for your browser
-  // - s-maxage=0: cache is fresh for 60 seconds on the edge, after which it becomes stale
-  // - stale-while-revalidate: allow serving stale content while revalidating on the edge
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control
+  // 快取設定
   cacheControlHeader: 'max-age=0, s-maxage=60, stale-while-revalidate',
-}
+
+  // !!! 這是處理 Step 3/3 UPN 錯誤的關鍵 !!!
+  // 如果程式碼允許修改 UPN 檢查邏輯，請將此行改為您登入的 Email
+  // (但由於我們不能確定您的程式碼是否會讀取這個變數，我們先保持預設)
+  userPrincipalName: 'nspool@vvwls.onmicrosoft.com', 
+};
