82990d0586d27f3d6c1b3090b305f3c387d55a38
nspool202307-dot <nspool202307@gmail.com>
2025-12-13 16:38:41 +0800

    Update api.config.js

--- config/api.config.js
+++ config/api.config.js
@@ -10,12 +10,12 @@
   module.exports = {
   // The clientId and clientSecret are used to authenticate the user with Microsoft Graph API using OAuth. You would
   // not need to change anything here if you can authenticate with your personal Microsoft account with OneDrive International.
-  clientId: 'f20fcec1-00d0-4811-b5a0-7260dffe1728',
-  obfuscatedClientSecret: 'U2FsdGVkX19JieV7uvU+DbFRMgZgXEVnG5HZY46uY/rApBcJA5lpduEU1IKLvI9V4ZQK3FxsHpWx5uhe3xZ2SQ==',
+  clientId: '',
+  obfuscatedClientSecret: '',
 
   // The redirectUri is the URL that the user will be redirected to after they have authenticated with Microsoft Graph API.
   // Likewise, you would not need to change redirectUri if you are using your personal Microsoft account with OneDrive International.
-  redirectUri: 'https://nspool-onedrive.pages.dev/api/auth/callback',
+  redirectUri: 'http://localhost',
 
   // These are the URLs of the OneDrive API endpoints. You would not need to change anything here if you are using OneDrive International
   // or E5 Subscription OneDrive for Business. You may need to change these if you are using OneDrive 世纪互联.
